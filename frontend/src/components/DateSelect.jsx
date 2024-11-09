8ea5259f37f7f85f7529f63d9836f111d29310e3
Cyril Joby <cyriljoby@dhcp-10-8-007-132.mobile.reshsg.uci.edu>
2024-11-08 22:59:28 -0800

    css and styling complete

--- frontend/src/components/DateSelect.jsx
+++ frontend/src/components/DateSelect.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAppContext } from "../AppContext";
+import "./DateSelect.css";
 
 function DateSelect() {
   const [selectedRange, setSelectedRange] = useState("all");
@@ -8,13 +9,20 @@ function DateSelect() {
   const handleChange = (event) => {
     const value = event.target.value;
     setSelectedRange(value);
-    handleFilterChange(value); 
+    handleFilterChange(value);
   };
 
   return (
-    <div>
-      <label htmlFor="filter-select">Filter by time range:</label>
-      <select id="filter-select" value={selectedRange} onChange={handleChange}>
+    <div className="date-select-card">
+      <label htmlFor="filter-select" className="date-select-label">
+        Filter by time range:
+      </label>
+      <select
+        id="filter-select"
+        value={selectedRange}
+        onChange={handleChange}
+        className="date-select-dropdown"
+      >
         <option value="all">All Time</option>
         <option value="week">Last Week</option>
         <option value="month">Last Month</option>
